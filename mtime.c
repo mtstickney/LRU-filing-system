03697487fb5b57982dccc8c3941ee9a65a600858
Matthew Stickney <fiendishlinux@gmail.com>
2011-12-10 18:20:29 -0500

    Add mtime utility

---
+++ mtime.c
@@ -0,0 +1,22 @@
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+
+int main(int argc, char **argv) {
+	int i;
+	struct stat s;
+
+	for (i=1; i<argc; i++) {
+		if (stat(argv[i], &s)) {
+			fprintf(stderr, "stat %s: ", argv[i]);
+			perror("");
+			errno = 0;
+		} else {
+			printf("%11u %s\n", s.st_mtime, argv[i]);
+		}
+	}
+
+	return 0;
+}
